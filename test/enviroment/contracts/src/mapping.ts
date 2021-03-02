6d2cc56793d430ced9dbbaa0e88d20828497ef1f
Ilnur Galiev <ilnur.a.g@gmail.com>
2021-03-02 05:15:19 +0300

    add call handler for contract

--- test/enviroment/contracts/src/mapping.ts
+++ test/enviroment/contracts/src/mapping.ts
@@ -1,4 +1,10 @@
 import { SyncCall } from '../generated/BlockNumStorage/BlockNumStorage'
+import { Sync } from '../generated/schema'
 
 export function handleSync(call: SyncCall): void {
+  let id = call.transaction.hash.toHex();
+  let sync = new Sync(id);
+  sync.key = call.inputs.key;
+  sync.value = call.outputs.value0;
+  sync.save();
 }
