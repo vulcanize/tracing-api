ce9cd841938eff5c9ac53908778c501473d78e22
Ilnur Galiev <ilnur.a.g@gmail.com>
2021-03-02 04:33:06 +0300

    add subgraph modification after deploy

--- test/enviroment/contracts/migrations/1_initial_migration.js
+++ test/enviroment/contracts/migrations/1_initial_migration.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+const yaml = require('yaml');
 const Migrations = artifacts.require("Migrations");
 const BlockNumStorage = artifacts.require("BlockNumStorage");
 
 async function main(deployer) {
-  await Promise.all([
-    deployer.deploy(Migrations),
-    deployer.deploy(BlockNumStorage),
-  ]);
+  await deployer.deploy(Migrations);
+  const store = await deployer.deploy(BlockNumStorage);
+
+  let subgraph = yaml.parse(fs.readFileSync("subgraph.yaml", {encoding:"utf-8"}));
+  subgraph.dataSources[0].source = store.address;
+  fs.writeFileSync("subgraph.yaml", yaml.stringify(subgraph))
 }
 
 module.exports = function (deployer) {
--- test/enviroment/contracts/truffle-config.js
+++ test/enviroment/contracts/truffle-config.js
@@ -16,6 +16,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
+      version: "0.8.1",
       settings: {
         optimizer: {
           enabled: true,
