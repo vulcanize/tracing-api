f444d4255ea9782308ffefb3a1f96d0e3fea0f34
Ilnur <ilnur.a.g@gmail.com>
2021-06-27 14:36:23 +0300

    add transaction debugger

--- pkg/eth/debug.go
+++ pkg/eth/debug.go
@@ -2,6 +2,7 @@ package eth
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -33,72 +34,124 @@ func (api *DebugAPI) WriteTxTraceGraph(ctx context.Context, hash common.Hash) (*
 	return nil, api.cache.SaveTxTraceGraph(data)
 }
 
-func (api *DebugAPI) TxTraceGraph(ctx context.Context, hash common.Hash) (*cache.TxTraceGraph, error) {
+func (api *DebugAPI) prepareEvm(ctx context.Context, hash common.Hash, ttracer vm.Tracer) (*vm.EVM, *transaction, error) {
 	tx, _, blockNum, txIndex, err := api.backend.GetTransaction(ctx, hash)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	signer := types.MakeSigner(api.backend.Config.ChainConfig, big.NewInt(int64(blockNum)))
 
 	block, err := api.backend.BlockByNumber(ctx, rpc.BlockNumber(blockNum))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	statedb, _, err := api.backend.StateAndHeaderByNumber(ctx, rpc.BlockNumber(blockNum-1))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	txs := block.Transactions()
 	for i, ln := uint64(0), uint64(len(txs)); i < ln && i < txIndex; i++ {
-		msg, err := tx.AsMessage(signer)
+		msg, err := txs[i].AsMessage(signer)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		evm, _ := api.backend.GetEVM(ctx, msg, statedb, block.Header())
 		_, err = core.ApplyMessage(evm, msg, new(core.GasPool).AddGas(math.MaxUint64))
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 	}
 
 	msg, err := tx.AsMessage(signer)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	statedb.SetBalance(msg.From(), math.MaxBig256)
 	vmctx := core.NewEVMBlockContext(block.Header(), api.backend, nil)
 	txContext := core.NewEVMTxContext(msg)
 
-	callTracer := tracer.NewCallTracer()
 	cfg := api.backend.Config.VmConfig
 	cfg.Debug = true
-	cfg.Tracer = callTracer
+	cfg.Tracer = ttracer
 
 	evm := vm.NewEVM(vmctx, txContext, statedb, api.backend.Config.ChainConfig, cfg)
-	_, err = core.ApplyMessage(evm, msg, new(core.GasPool).AddGas(math.MaxUint64))
+	internalTx := transaction{
+		Message:   msg,
+		Index:     txIndex,
+		BlockHash: block.Hash(),
+		BlockNumb: blockNum,
+	}
+	return evm, &internalTx, nil
+}
+
+func (api *DebugAPI) TxTraceGraph(ctx context.Context, hash common.Hash) (*cache.TxTraceGraph, error) {
+	degugger := tracer.NewCallTracer()
+
+	evm, tx, err := api.prepareEvm(ctx, hash, degugger)
 	if err != nil {
 		return nil, err
 	}
 
-	msg, _ = tx.AsMessage(signer)
+	_, err = core.ApplyMessage(evm, tx.Message, new(core.GasPool).AddGas(math.MaxUint64))
+	if err != nil {
+		return nil, err
+	}
+
 	frames := []tracer.Frame{
 		{
 			Op:     vm.CALL,
-			From:   msg.From(),
-			To:     *msg.To(),
-			Input:  msg.Data(),
-			Output: callTracer.Output(),
-			Gas:    msg.Gas(),
-			Cost:   msg.Gas(),
-			Value:  msg.Value(),
+			From:   tx.Message.From(),
+			To:     *tx.Message.To(),
+			Input:  tx.Message.Data(),
+			Output: degugger.Output(),
+			Gas:    tx.Message.Gas(),
+			Cost:   tx.Message.Gas(),
+			Value:  tx.Message.Value(),
 		},
 	}
-	callFrames := callTracer.Frames()
+
 	return &cache.TxTraceGraph{
 		TxHash:      hash,
-		TxIndex:     txIndex,
-		BlockHash:   block.Hash(),
-		BlockNumber: blockNum,
-		Frames:      append(frames, callFrames...),
+		TxIndex:     tx.Index,
+		BlockHash:   tx.BlockHash,
+		BlockNumber: tx.BlockNumb,
+		Frames:      append(frames, degugger.Frames()...),
+	}, nil
+}
+
+func (api *DebugAPI) TraceTransaction(ctx context.Context, hash common.Hash) (*ExecutionResult, error) {
+	degugger := vm.NewStructLogger(&vm.LogConfig{
+		DisableMemory:     false,
+		DisableStack:      false,
+		DisableStorage:    false,
+		DisableReturnData: false,
+		Debug:             true,
+		Limit:             0,
+	})
+
+	evm, tx, err := api.prepareEvm(ctx, hash, degugger)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := core.ApplyMessage(evm, tx.Message, new(core.GasPool).AddGas(math.MaxUint64))
+	if err != nil {
+		return nil, err
+	}
+
+	returnVal := fmt.Sprintf("%x", result.Return())
+	if len(result.Revert()) > 0 {
+		returnVal = fmt.Sprintf("%x", result.Revert())
+	}
+
+	return &ExecutionResult{
+		Gas:         result.UsedGas,
+		Failed:      result.Failed(),
+		ReturnValue: returnVal,
+		StructLogs:  FormatLogs(degugger.StructLogs()),
 	}, nil
 }
---
+++ pkg/eth/internal.go
@@ -0,0 +1,75 @@
+package eth
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+type transaction struct {
+	Message   types.Message
+	Index     uint64
+	BlockHash common.Hash
+	BlockNumb uint64
+}
+
+type ExecutionResult struct {
+	Gas         uint64         `json:"gas"`
+	Failed      bool           `json:"failed"`
+	ReturnValue string         `json:"returnValue"`
+	StructLogs  []StructLogRes `json:"structLogs"`
+}
+
+// StructLogRes stores a structured log emitted by the EVM while replaying a
+// transaction in debug mode
+type StructLogRes struct {
+	Pc      uint64             `json:"pc"`
+	Op      string             `json:"op"`
+	Gas     uint64             `json:"gas"`
+	GasCost uint64             `json:"gasCost"`
+	Depth   int                `json:"depth"`
+	Error   error              `json:"error,omitempty"`
+	Stack   *[]string          `json:"stack,omitempty"`
+	Memory  *[]string          `json:"memory,omitempty"`
+	Storage *map[string]string `json:"storage,omitempty"`
+}
+
+// FormatLogs formats EVM returned structured logs for json output
+func FormatLogs(logs []vm.StructLog) []StructLogRes {
+	formatted := make([]StructLogRes, len(logs))
+	for index, trace := range logs {
+		formatted[index] = StructLogRes{
+			Pc:      trace.Pc,
+			Op:      trace.Op.String(),
+			Gas:     trace.Gas,
+			GasCost: trace.GasCost,
+			Depth:   trace.Depth,
+			Error:   trace.Err,
+		}
+		if trace.Stack != nil {
+			stack := make([]string, len(trace.Stack))
+			for i, stackValue := range trace.Stack {
+				stack[i] = fmt.Sprintf("%x", math.PaddedBigBytes(stackValue, 32))
+			}
+			formatted[index].Stack = &stack
+		}
+		if trace.Memory != nil {
+			memory := make([]string, 0, (len(trace.Memory)+31)/32)
+			for i := 0; i+32 <= len(trace.Memory); i += 32 {
+				memory = append(memory, fmt.Sprintf("%x", trace.Memory[i:i+32]))
+			}
+			formatted[index].Memory = &memory
+		}
+		if trace.Storage != nil {
+			storage := make(map[string]string)
+			for i, storageValue := range trace.Storage {
+				storage[fmt.Sprintf("%x", i)] = fmt.Sprintf("%x", storageValue)
+			}
+			formatted[index].Storage = &storage
+		}
+	}
+	return formatted
+}
